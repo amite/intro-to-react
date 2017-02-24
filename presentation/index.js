f70bf22c484697b9ea0a5d11000bfb73048def2f
Amit Erandole <amit.erandole@gmail.com>
2017-02-24 22:37:34 +0530

    deleted extra content

--- presentation/index.js
+++ presentation/index.js
@@ -412,56 +412,6 @@ export default class Presentation extends React.Component {
           </Heading>
           <Link href="http://bit.ly/more-js" target="_blank">http://bit.ly/more-js</Link>
         </Slide>
-
-
-        <Slide transition={["slide"]} bgColor="primary">
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            
-          </Text>
-          <Layout>
-            <Fill>
-              <List>
-                <Appear><ListItem>Item 1</ListItem></Appear>
-                <Appear><ListItem>Item 1</ListItem></Appear>
-                <Appear><ListItem>Item 1</ListItem></Appear>
-                <Appear><ListItem>Item 1</ListItem></Appear>
-              </List>
-            </Fill>
-            <Fill>
-              <List>
-                <Appear><ListItem>Item 1</ListItem></Appear>
-                <Appear><ListItem>Item 1</ListItem></Appear>
-                <Appear><ListItem>Item 1</ListItem></Appear>
-                <Appear><ListItem>Item 1</ListItem></Appear>
-              </List>
-            </Fill>
-          </Layout>
-          
-        </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
       </Deck>
     );
   }
